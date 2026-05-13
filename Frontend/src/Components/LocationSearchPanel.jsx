0f3370a061c4f2201d32441b8a9b7238372a0d93
Ajinkya Mane <maneajinkya8906@gmail.com>
2026-05-13 21:25:16 +0530

    LocationSearchPanel added to Home.jsx and App.jsx

--- Frontend/src/Components/LocationSearchPanel.jsx
+++ Frontend/src/Components/LocationSearchPanel.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import MapPin from 'lucide-react'
 
 const LocationSearchPanel = () => {
   return (
     <div>
+      <MapPin />
         <h4>Mumbai , Maharashtra , India</h4>
     </div>
   )
